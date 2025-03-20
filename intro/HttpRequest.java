da9c199d48b7590491950ffc16420b23a0036965
mallikarjuna748 <rmallikarjuna748@gmail.com>
2025-03-20 22:29:30 +0530

    HTTPRequest

--- intro/HttpRequest.java
+++ intro/HttpRequest.java
@@ -5,14 +5,15 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 
 
 public class HttpRequest {
-	@Test
+//	@Test(priority=1)
 	void listUsers()
-	{
+	{	
 		given()
-		
 		
 		.when()
 		.get("https://reqres.in/api/users?page=2")
@@ -20,6 +21,24 @@ public class HttpRequest {
 			.statusCode(200)
 			.body("page",equalTo(2))
 			.log().all();
+	}
+	@Test
+	void createUser()
+	{
+		HashMap<String, String> data = new HashMap<>();
+		data.put("name", "Somu");
+		data.put("job","Software");
+		
+	 given()
+	 	.contentType("application/json")
+	 	.body(data)
+	 
+	 .when()
+	 	.post("https://reqres.in/api/users")
+	 .then()
+	 	.statusCode(201)
+	 	.log().all();
+	 
 	}
 	
 
