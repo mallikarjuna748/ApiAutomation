da668c428a3e4ee6c9a94bdc3209b4d87cd68754
mallikarjuna748 <rmallikarjuna748@gmail.com>
2025-03-20 19:02:16 +0530

    GET

---
+++ intro/HttpRequest.java
@@ -0,0 +1,26 @@
+package intro;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class HttpRequest {
+	@Test
+	void listUsers()
+	{
+		given()
+		
+		
+		.when()
+		.get("https://reqres.in/api/users?page=2")
+		.then()
+			.statusCode(200)
+			.body("page",equalTo(2))
+			.log().all();
+	}
+	
+
+}
